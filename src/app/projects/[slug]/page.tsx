8beeeab6650211d724e36f9e1852d83ea3a8b968
Avily Silva <avilyre@gmail.com>
2024-05-20 13:15:47 -0300

    feat: add header section

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,89 @@
+import { Figma, Github, Globe } from "lucide-react";
+
+import { projects } from "@/@data/projects";
+import { PageHeader } from "@/components/page-header";
+
+import { ProjectDetails } from "./interface";
+import { strings } from "./strings";
+
+const ProjectDetails = (props: Readonly<ProjectDetails>) => {
+  const {
+    params: { slug },
+  } = props;
+
+  const project = projects.filter(project => project.slug === slug).shift();
+
+  if (!project) return;
+
+  return (
+    <main>
+      <header>
+        <div className="mb-8 aspect-video w-full rounded-md bg-tertiary sm:aspect-[16/5.68]"></div>
+        <div className="flex flex-col gap-8 sm:flex-row">
+          <section className="max-w-[488px] flex-1">
+            <PageHeader
+              title={project.title}
+              description={project.description}
+            />
+
+            <div className="flex items-center gap-8">
+              <a
+                href={project.links?.figma}
+                target="_blank"
+                title={strings.figma}
+                className="inline-block rounded-md bg-tertiary p-3 text-secondary transition-colors hover:text-primary"
+              >
+                <Figma />
+              </a>
+              <a
+                href={project.links?.deploy}
+                target="_blank"
+                title={strings.deploy}
+                className="inline-block rounded-md bg-tertiary p-3 text-secondary transition-colors hover:text-primary"
+              >
+                <Globe />
+              </a>
+              <a
+                href={project.links?.github}
+                target="_blank"
+                title={strings.github}
+                className="inline-block rounded-md bg-tertiary p-3 text-secondary transition-colors hover:text-primary"
+              >
+                <Github />
+              </a>
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-8">
+            <h5 className="select-none">
+              <span className="block text-sm text-secondary">
+                {strings.myRole}
+              </span>
+              <span className="text-base leading-tight text-primary">
+                {project.details.role}
+              </span>
+            </h5>
+            <h5 className="select-none">
+              <span className="block text-sm text-secondary">
+                {strings.developmentTime}
+              </span>
+              <span className="text-base leading-tight text-primary">
+                {project.details.developmentTime}
+              </span>
+            </h5>
+            <h5 className="select-none">
+              <span className="block text-sm text-secondary">
+                {strings.projectType}
+              </span>
+              <span className="text-base leading-tight text-primary">
+                {project.details.type}
+              </span>
+            </h5>
+          </section>
+        </div>
+      </header>
+    </main>
+  );
+};
+
+export default ProjectDetails;
---
+++ src/app/projects/[slug]/strings.ts
@@ -0,0 +1,8 @@
+export const strings = {
+  figma: "figma",
+  github: "github",
+  deploy: "ver online",
+  myRole: "minha função",
+  developmentTime: "tempo estimado",
+  projectType: "tipo de projeto",
+};
