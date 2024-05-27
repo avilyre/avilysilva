1957d3302a8f716ca345e7940b1cb7ce502e138d
Avily Silva <avilyre@gmail.com>
2024-05-27 17:27:31 -0300

    feat: add keen slider to gallery component

--- src/app/projects/[slug]/components/gallery.tsx
+++ src/app/projects/[slug]/components/gallery.tsx
@@ -1,32 +1,63 @@
 "use client";
 
+import "keen-slider/keen-slider.min.css";
+
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
 import { Fragment, useState } from "react";
 
 export const Gallery = (props: { images: string[] }) => {
   const { images } = props;
 
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+
+  const [keenSliderRef, keenSliderInstanceRef] = useKeenSlider({
+    slides: {
+      origin: "auto",
+      perView: 1.5,
+      spacing: 16,
+    },
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    breakpoints: {
+      "(max-width: 640px)": {
+        slides: {
+          perView: 1,
+        },
+      },
+    },
+  });
+
+  const genericSliderArraySize = [
+    ...Array(
+      keenSliderInstanceRef.current?.track.details.slides.length ||
+        images.length,
+    ).keys(),
+  ];
+
   const defaultImageSize = {
     height: 276,
     width: 528,
   };
 
-  const [currentNumberImage, setCurrentNumberImage] = useState<number>(0);
-
   const handleBulletClick = (index: number) => {
-    setCurrentNumberImage(index);
+    keenSliderInstanceRef.current?.moveToIdx(index);
   };
 
   return (
     <Fragment>
-      <div className="no-scrollbar flex w-full select-none snap-x gap-8 overflow-x-auto scroll-smooth">
+      <div
+        ref={keenSliderRef}
+        className="keen-slider no-scrollbar flex w-full select-none"
+      >
         {images.map((image, index) => (
           <Image
             key={index}
             src={image}
             id={`gallery-image-${index}`}
             alt={`Project image ${index + 1}`}
-            className="snap-start rounded-md"
+            className={`keen-slider__slide rounded-md`}
             tabIndex={0}
             width={defaultImageSize.width}
             height={defaultImageSize.height}
@@ -35,13 +66,12 @@ export const Gallery = (props: { images: string[] }) => {
         ))}
       </div>
       <div className="mt-6 flex items-center justify-center gap-4">
-        {images.map((_, index) => (
-          <a
+        {genericSliderArraySize.map(index => (
+          <button
             key={index}
-            href={`#gallery-image-${index}`}
-            className={`h-[13px] w-[32px] rounded-full ${currentNumberImage === index ? "bg-blue-500" : "bg-tertiary"}`}
+            className={`h-[13px] w-[32px] rounded-full transition-[background-color] ${currentSlide === index ? "bg-blue-500" : "bg-tertiary"}`}
             onClick={() => handleBulletClick(index)}
-          ></a>
+          ></button>
         ))}
       </div>
     </Fragment>
