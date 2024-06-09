fcb03e06a24ebd2023fd8cc410649fa7b82c2b74
Avily Silva <avilyre@gmail.com>
2024-06-09 07:46:44 -0300

    feat: add projects page skeleton

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
-import { Card } from "@/components/card";
-import { CardPlaceholder } from "@/components/card-placeholder";
+import { Suspense } from "react";
+
 import { PageHeader } from "@/components/page-header";
-import { getDynamicPlaceholder } from "@/services/get-dynamic-placeholder.service";
-import { getAllProjects } from "@/services/projects.service";
 import { generateSEO } from "@/utility/generate-seo";
 
+import { ProjectsList } from "./sections/projects-list";
+import { ProjectsListSkeleton } from "./sections/projects-list/skeleton";
 import { strings } from "./strings";
 
 export const metadata = generateSEO({
@@ -12,16 +12,7 @@ export const metadata = generateSEO({
   description: strings.description,
 });
 
-const Projects = async () => {
-  const projects = await getAllProjects();
-
-  const defaultPlaceQuantityWithoutProjects = 4;
-  const defaultPlaceholderQuantity = 3;
-  const placeholdersToBeRendered =
-    projects.length > 0
-      ? Math.round(defaultPlaceholderQuantity / projects.length)
-      : defaultPlaceQuantityWithoutProjects;
-
+const Projects = () => {
   return (
     <main>
       <PageHeader
@@ -34,35 +25,9 @@ const Projects = async () => {
           {strings.selected}
         </h2>
 
-        <div
-          data-testid="projects"
-          className="grid grid-cols-1 gap-8 sm:grid-cols-2"
-        >
-          {projects.map(async ({ id, data: project }) => {
-            const imagePlaceholder = await getDynamicPlaceholder(
-              project.image.url as string,
-            );
-
-            return (
-              <Card
-                key={id}
-                title={project.title as string}
-                description={project.description as string}
-                image={{
-                  src: project.image.url as string,
-                  alt: project.image.alt as string,
-                  placeholderBlur: imagePlaceholder.base64,
-                }}
-                slug={project.slug as string}
-                typeURL="project"
-              />
-            );
-          })}
-
-          {[...Array(placeholdersToBeRendered)].map((_, index) => (
-            <CardPlaceholder key={index} />
-          ))}
-        </div>
+        <Suspense fallback={<ProjectsListSkeleton />}>
+          <ProjectsList />
+        </Suspense>
       </section>
     </main>
   );
