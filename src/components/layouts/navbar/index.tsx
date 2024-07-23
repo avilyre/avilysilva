c1ce6fd4c330820c3f6ba561ca6ad0ee115012f3
Avily Silva <avilyre@gmail.com>
2024-07-23 13:05:04 -0300

    feat: add nav item component

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,21 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+
+import { NavItem } from "@/components/NavItem";
 
 import { strings } from "./strings";
 
 export const Navbar = () => {
-  const pathname = usePathname();
-
-  const linkClassesBasedOnActive = (patname: string) => {
-    const activeLinkClasses =
-      "relative text-primary after:absolute after:left-1/2 after:top-1/2 after:z-[-1] after:h-[170%] after:w-[130%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md after:bg-slate-900 after:content-['']";
-    const classesResult =
-      pathname === patname ? activeLinkClasses : "text-secondary";
-    return classesResult;
-  };
-
   return (
     <nav
       data-testid="navbar"
@@ -29,18 +20,8 @@ export const Navbar = () => {
       </Link>
 
       <div className="flex gap-8">
-        <Link
-          href="/about"
-          className={`text-base ${linkClassesBasedOnActive("/about")} underline-offset-8 transition-colors hover:text-primary`}
-        >
-          {strings.about}
-        </Link>
-        <Link
-          href="/projects"
-          className={`text-base ${linkClassesBasedOnActive("/projects")} underline-offset-8 transition-colors hover:text-primary`}
-        >
-          {strings.projects}
-        </Link>
+        <NavItem href="/about">{strings.about}</NavItem>
+        <NavItem href="/projects">{strings.projects}</NavItem>
       </div>
     </nav>
   );
