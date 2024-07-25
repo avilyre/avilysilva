36b9c947ca38cb496ddb58bbc6db31ae8674ede1
Avily Silva <avilyre@gmail.com>
2024-07-25 12:50:11 -0300

    fix: add verification if projects list is empty or not

--- cypress/e2e/projects-details.cy.ts
+++ cypress/e2e/projects-details.cy.ts
@@ -1,16 +1,22 @@
 describe("Project Details Page", () => {
   it("should be able to render the page", () => {
     cy.visit("http://localhost:3000/projects");
-    const projectCard = cy.get("[data-testid='card']").should("exist");
-    projectCard.click();
 
-    const projectDetailsMainElement = cy.get("[data-testid='project-details']");
-    projectDetailsMainElement.should("exist");
+    cy.get("body").then(body => {
+      if (body.find("[data-testid='card']").length > 0) {
+        cy.get("[data-testid='card']").first().click();
 
-    cy.fixture("project-details.json").then(({ strings }) => {
-      const generalTitleSectionElement = cy.get("header h2");
-      generalTitleSectionElement.should("exist");
-      generalTitleSectionElement.contains(strings.general);
+        const projectDetailsMainElement = cy.get(
+          "[data-testid='project-details']",
+        );
+        projectDetailsMainElement.should("exist");
+
+        cy.fixture("project-details.json").then(({ strings }) => {
+          const generalTitleSectionElement = cy.get("header h2");
+          generalTitleSectionElement.should("exist");
+          generalTitleSectionElement.contains(strings.general);
+        });
+      }
     });
   });
 });
